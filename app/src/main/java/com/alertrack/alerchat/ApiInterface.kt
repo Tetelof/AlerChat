484ab27929de23c92e38d9cce2b421d64fe499a5
Tetelof <94086307+Tetelof@users.noreply.github.com>
2022-05-06 13:10:04 -0300

    Dia 4 - Retrofit

--- app/src/main/java/com/alertrack/alerchat/ApiInterface.kt
+++ app/src/main/java/com/alertrack/alerchat/ApiInterface.kt
@@ -6,7 +6,20 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
-interface ApiInterface {
+interface RetrofitService {
     @POST("/api/teste_mobile/auth.php")
-    fun createPost(@Body login: Login) : Call<Auth>
+    fun createPost(@Body login: Login) : Call<Autenticacao>
+    
+    companion object{
+        private val retrofitService : RetrofitService by lazy {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("http://alertrack.com.br/")
+                .addConverterFactory(GsonConverteractory.create())
+                .build()
+            retrofit.create(RetrofitService::class.java)
+        }
+        fun getRetrofitInstance() : RetrofitService {
+            return retrofitService
+        }
+    }
 }
